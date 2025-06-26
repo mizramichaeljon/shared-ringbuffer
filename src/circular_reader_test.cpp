1e8fe4d619d8370caea9be80d7d52eadff7214c8
mizramichaeljon <mmizra@live.co.uk>
2025-06-26 14:31:40 +0100

    updated readme - merged test files

--- src/circular_reader_test.cpp
+++ src/circular_reader_test.cpp
@@ -1,4 +1,5 @@
 #include "../include/SharedRingBufferReader.hpp"
+#include <fstream>
 #include <chrono>
 #include <thread>
 #include <iostream>
@@ -7,40 +8,46 @@
 
 std::atomic<bool> running(true);
 
-void handle_sigint(int)
-{
+void handle_sigint(int) {
     std::cout << "\n[Signal] SIGINT received. Exiting...\n";
     running = false;
 }
 
 int main()
 {
-    std::cout << "[Main] Entered ringbuffer real-time reader\n";
+    std::cout << "[Main] Entered reader test main()\n";
     std::signal(SIGINT, handle_sigint); // Handle Ctrl+C
 
     SharedRingBufferReader reader("ringbuffer_audio");
 
+    constexpr int sampleRate = 48000;
     constexpr int blockSize = 1024;
     constexpr int pollingIntervalMs = 16; // ~60Hz
+
+    // Open CSV in append mode
+    std::ofstream csv("out_combined.csv", std::ios::app);
+    if (!csv.is_open()) {
+        std::cerr << "[Error] Failed to open out_combined.csv\n";
+        return 1;
+    }
 
     while (running)
     {
         std::vector<float> out = reader.getLatestSamples(blockSize);
 
-        if (out.empty())
-        {
-            std::cerr << "[Reader] WARNING: Empty sample buffer\n";
-        }
-        else
-        {
+        if (out.empty()) {
+            std::cerr << "[Reader] WARNING: Received empty sample buffer\n";
+        } else {
             std::cout << "[Reader] First: " << out.front()
-                      << ", Last: " << out.back()
-                      << ", Size: " << out.size() << "\n";
+                      << ", Last: " << out.back() << "\n";
+
+            for (float f : out) csv << f << "\n";
         }
 
         std::this_thread::sleep_for(std::chrono::milliseconds(pollingIntervalMs));
     }
 
+    csv.close();
     std::cout << "[Main] Reader exited cleanly.\n";
     return 0;
 }
